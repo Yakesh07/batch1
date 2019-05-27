8fe3592ad997e429fd66649b34cb263232f411a4
Yakesh Kumar <rakhesh.stp@gmail.com>
2019-05-26 21:47:01 -0700

    Added a File

---
+++ src/main/java/com/app/pages/Homepage.java
@@ -0,0 +1,8 @@
+package com.app.pages;
+
+
+
+public class Homepage extends Basepage{
+	
+
+}
---
+++ src/test/java/com/app/test/HomeTestpage.java
@@ -0,0 +1,11 @@
+package com.app.test;
+
+import com.app.pages.Homepage;
+
+public class HomeTestpage extends Homepage{
+	
+
+
+	}
+	
+
